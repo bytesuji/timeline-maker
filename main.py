a4bcbc086767e4492e39a104cd2b820486f2a989
Albert Brox III <albert@exypno.tech>
2017-03-27 14:42:32 -0400

    adjusted spacing

--- main.py
+++ main.py
@@ -25,18 +25,21 @@ framework_string = r'''\documentclass[border=10pt]{standalone}
 '''
 
 def init_file(name, tex=framework_string):
+    
     '''Creates a .tex file which is the basis of the entire timeline. Run only once.'''
 
     main_file = open(name + '.tex', 'w')
     main_file.write(tex)
 
 def create(name, engine='xelatex'):
+
     '''Compiles the .tex file into a .pdf using the engine specified in the parameters, then displays it using the evince pdf reader.'''
 
     call([engine, name + '.tex'])
     call(['evince', name + '.pdf']) # if you want, change 'evince' to the pdf viewer of your choice
 
 def add_phase(start_week, end_week, in_val, string=framework_string, color='red', involvement_degree='2.5cm'):
+
     new_string = string;
 
     index = new_string.find('\phase')
@@ -46,10 +49,14 @@ def add_phase(start_week, end_week, in_val, string=framework_string, color='red'
     return new_string
 
 if __name__ == '__main__':
+
     import sys
 
     name = sys.argv[1]
     tex = add_phase(2,3,0.5,framework_string,'blue','5.6cm')
     tex = add_phase(4,5,0.1,tex,'orange','3cm')
+
+
     init_file(name, tex)
+
     create(name)
